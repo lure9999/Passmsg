e2c2c9d28315f2a5e01821b0d7324335051ada1e
lure <lure_9999@163.com>
2015-03-25 14:53:03 +0800

    lure
    passmsg

---
+++ passmsg/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  passmsg
+//
+//  Created by Liu Zhichao on 15-3-25.
+//  Copyright (c) 2015年 LiuZhiChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIViewController *viewController;
+
+@end
+
+
---
+++ passmsg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  passmsg
+//
+//  Created by Liu Zhichao on 15-3-25.
+//  Copyright (c) 2015年 LiuZhiChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
